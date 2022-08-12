094119e230cba3a2956a1cc264b57cc849f49dc9
JoseFernandes975 <Zehenrique752005@gmail.com>
2022-08-12 13:21:01 -0300

    Padrão de projeto OBSERVER para atualizar automaticamente

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -25,7 +26,7 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentServices;
 
-public class DepartmentListController implements Initializable{
+public class DepartmentListController implements Initializable, DataChangeListener{
 	
 	private DepartmentServices services;
 	
@@ -81,6 +82,7 @@ public class DepartmentListController implements Initializable{
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
 			controller.setDepartmentServices(new DepartmentServices());
+			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
 			
 			
@@ -96,6 +98,11 @@ public class DepartmentListController implements Initializable{
 		catch(IOException e) {
 			Alerts.showAlert("IOException", "Error window", e.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	@Override
+	public void onDataChanged() {
+	  updateTableView();
 	}
 
 }
